b73284094cc3b8e9576e5522f89d9ff29c2c79f0
wiredline <max.ataev.it@google.com>
2026-04-22 09:46:06 +0000

    feat: raw pointers deleted, added PGresultPtr and PGconnPtr as smart pointers

--- src/include/PgClient.hpp
+++ src/include/PgClient.hpp
@@ -4,42 +4,39 @@
 #include <postgresql/libpq-fe.h>
 #include <stdexcept>
 #include <string>
+#include <memory>
+
+using PGconnPtr = std::unique_ptr<PGconn, decltype(&PQfinish)>;
+using PGResultPtr = std::unique_ptr<PGresult, decltype(&PQclear)>;
 
 class PgClient{
 public:
-    explicit PgClient(const std::string& conninfo){
-        conn_ = PQconnectdb(conninfo.c_str());
-
-        if(PQstatus(conn_) != CONNECTION_OK){
-            std::string error = PQerrorMessage(conn_);
-            PQfinish(conn_);
-            throw std::runtime_error("DB connection failed: " + error);
+    explicit PgClient(const std::string& conninfo)
+    :conn_(PQconnectdb(conninfo.c_str()), &PQfinish)
+    {
+        if (!conn_ || PQstatus(conn_.get()) != CONNECTION_OK) {
+            throw std::runtime_error("Connection to db failed");
         }
     }
 
-    ~PgClient(){ //destructor
-        if(conn_) PQfinish(conn_);
-    }
+    ~PgClient() = default; //destructor
     PgClient(const PgClient&) = delete;//copy constr
-    PgClient& operator=(const PgClient&) =  delete;//copy assig
-    PgClient(const PgClient&&) = delete;//move constr
-    PgClient& operator=(const PgClient&&)=delete;//move assig
+    PgClient& operator=(const PgClient&)  =  delete;//copy assig
+    PgClient(PgClient&&) noexcept = delete;//move constr
+    PgClient& operator=(PgClient&&) noexcept = delete;//move assig
 
-    PGresult* Exec(const std::string& query){
-        PGresult* res = PQexec(conn_, query.c_str());
-        if(!res){
-            throw std::runtime_error("Query execution fail");
-        }
-        if(PQresultStatus(res) != PGRES_COMMAND_OK&&
-        PQresultStatus(res) != PGRES_TUPLES_OK){
-            std::string err = PQerrorMessage(conn_);
-            PQclear(res);
-            throw std::runtime_error(err);
-        }
+    PGResultPtr Exec(const std::string& query){
+        PGResultPtr res(PQexec(conn_.get(), query.c_str()), &PQclear);
+
+        if(!res) throw std::runtime_error("Query execution fail");
+
+        auto status = PQresultStatus(res.get());
+        if(status != PGRES_COMMAND_OK && status != PGRES_TUPLES_OK) throw std::runtime_error(std::string(PQerrorMessage(conn_.get())));
+
         return res;
     }
-    PGconn* GetRaw(){return conn_;}
+    PGconn* GetRaw(){return conn_.get();}
 private:
-    PGconn* conn_;
+    PGconnPtr conn_;
 };
 #endif
